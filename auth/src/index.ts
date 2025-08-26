5dc6eae4e14356f9ad81595eecf7febf85d9d7ca
ayushblaze <ayushmoca@gmail.com>
2025-08-26 17:43:47 +0530

    Ingress: setup ingress for project

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Current User: Ayush (GOAT)");
+});
+
 app.listen(3000, () => {
   console.log("Auth Service Listening on Port: 3000 ✅");
 });
