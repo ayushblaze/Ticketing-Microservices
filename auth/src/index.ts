cfdcfed66abb6f63a72a070b59423389c87a38e9
ayushblaze <ayushmoca@gmail.com>
2025-08-27 15:41:57 +0530

    feat(error-handling): setup custom error handler class and take care of async errors too

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import 'express-async-errors';
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +14,13 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.get('*', async (req, res) => {
+  // next(new NotFoundError());
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Auth Service Listening on Port: 3000 ✅");
