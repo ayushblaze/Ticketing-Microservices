459a0df1bac466fa60b3191248fd927986b4a478
ayushblaze <ayushmoca@gmail.com>
2025-08-29 11:35:38 +0530

    feat(authentication): setup cookies and generate jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import 'express-async-errors';
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -9,7 +10,12 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set('trust proxy', true); // since traffic is coming from the nginx proxy, express might not trust this https connection
 app.use(express.json());
+app.use(cookieSession({
+  signed: false, // not needed since JWT itself is cryptographically signed and verified
+  secure: true, // only send cookies over https connection
+}));
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,6 +30,8 @@ app.get('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error('JWT_KEY is not defined :(');
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
