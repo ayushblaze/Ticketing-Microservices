53529b37b0c0d6e529011736ecb583c97ac9c36b
ayushblaze <ayushmoca@gmail.com>
2025-09-14 18:19:36 +0530

    refactor: refactor index file for test architecture

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,33 +1,5 @@
-import express from "express";
-import 'express-async-errors';
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.set('trust proxy', true); // since traffic is coming from the nginx proxy, express might not trust this https connection
-app.use(express.json());
-app.use(cookieSession({
-  signed: false, // not needed since JWT itself is cryptographically signed and verified
-  secure: true, // only send cookies over https connection
-}));
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.get('*', async (req, res) => {
-  // next(new NotFoundError());
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY is not defined :(');
